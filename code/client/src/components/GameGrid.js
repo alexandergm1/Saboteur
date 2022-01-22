ea404d06cb05d7cd8a98b1e750ba4c63b4de58e5
petermordaunt <peetfire@hotmail.com>
2022-01-22 10:33:41 +0000

    sort of working but not :-(

--- code/client/src/components/GameGrid.js
+++ code/client/src/components/GameGrid.js
@@ -7,9 +7,9 @@ function GameGrid({gridState}) {
 
     // const delay = ms => new Promise((res) => setTimeout(res,ms))
     // await delay(7000)
-
+    console.log(gridState)
         const gridNodes = gridState.map((row, rowIndex) => {
-        // console.log(row)
+        
         return row.map((item, colIndex) => {
             console.log(item, rowIndex, colIndex)
             return (<GridItem  row = {rowIndex} col={colIndex} item={item} key = {rowIndex.toString() + colIndex.toString()}/>)
--- code/client/src/components/GridItem.js
+++ code/client/src/components/GridItem.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 function GridItem({row, col, item}) {
+  
+  let dummyVal = "true"
+  if(item.inverted === false){ dummyVal = "false"}
+
   return <div className = "grid-item">
     <div id={row.toString() + "-" + col.toString()}>
-    {row.toString() + "-" + col.toString() }
-    {item["inverted"]}
+    {row.toString() + "-" + col.toString()}<br></br>
+    {dummyVal}
     </div>
   </div>;
 }
