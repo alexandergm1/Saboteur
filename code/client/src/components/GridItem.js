96d7aa642fe23983b8b02792668a652b4d04ae78
alexandergm1 <alexander.1gmac@gmail.com>
2022-01-22 12:41:50 +0000

    Amended syntax in seeds.js

--- code/client/src/components/GridItem.js
+++ code/client/src/components/GridItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import "../css/GridItems.css"
 
+
 function GridItem({row, col, item}) {
+  
+  let dummyVal = "true"
+  if(item.inverted === false){ dummyVal = "false"}
+
   return <div className = "grid-item">
+    <img src={item.image_url} alt="start-card"></img>
     <div id={row.toString() + "-" + col.toString()}>
-    <img src={item["image url"]}></img>
-    {row.toString() + "-" + col.toString() }
-    {item["inverted"]}
+    {row.toString() + "-" + col.toString()}<br></br>
+    {dummyVal}
     </div>
   </div>;
 }
