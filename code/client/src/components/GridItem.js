c2b0b123973daa30e6e5bce34a077267f663a92a
alexandergm1 <alexander.1gmac@gmail.com>
2022-01-22 13:56:37 +0000

    tile image displaying correctly

--- code/client/src/components/GridItem.js
+++ code/client/src/components/GridItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "../css/GridItems.css"
+import "../css/Sizing.css"
 
 
 function GridItem({row, col, item}) {
@@ -7,7 +8,7 @@ function GridItem({row, col, item}) {
   let dummyVal = "true"
   if(item.inverted === false){ dummyVal = "false"}
 
-  return <div className = "grid-item" style={{backgroundImage: `url(${item.image_url})`}}>
+  return <div className = "grid-item" style={{backgroundImage: `url(${item.image_url})`, backgroundSize: 'cover'}}>
     <div id={row.toString() + "-" + col.toString()}>
     {row.toString() + "-" + col.toString()}<br></br>
     {dummyVal}
