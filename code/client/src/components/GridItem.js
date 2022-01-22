6d509f5e62726b0784ea67f0a6c49296db973239
alexandergm1 <alexander.1gmac@gmail.com>
2022-01-22 12:11:44 +0000

    Added css positions for grid items

--- code/client/src/components/GridItem.js
+++ code/client/src/components/GridItem.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import "../css/GridItems.css"
 
 function GridItem({row, col, item}) {
   return <div className = "grid-item">
     <div id={row.toString() + "-" + col.toString()}>
+    <img src={item["image url"]}></img>
     {row.toString() + "-" + col.toString() }
     {item["inverted"]}
     </div>
