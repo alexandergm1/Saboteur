a8435cfd108f3548f9d79abfe0c8a554017ce3b9
alexandergm1 <alexander.1gmac@gmail.com>
2022-01-22 13:31:02 +0000

    Added fixed code

--- code/client/src/containers/GameContainer.js
+++ code/client/src/containers/GameContainer.js
@@ -1,14 +1,20 @@
 import React,{useEffect, useState} from 'react';
 import GameGrid from '../components/GameGrid';
+import Loading from '../components/Loading';
+
+import {getData} from '../GameService'
+
+function GameContainer() {
   
-  const [data, setData] = useState();
+  const [data, setData] = useState({});
   const [loading, setLoading] = useState(true)
-  const [cards, setCards] = useState([])
+  const [gameActive, setGameActive] = useState(false)
+  const [cards, setCards] = useState(null)
   const [gridState, setGridState] = useState([
       [ {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
       [ {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}], 
       [ {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}], 
-      [ {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
+      [ {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}], 
       [ {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}], 
       [ {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}], 
       [ {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]    
@@ -17,49 +23,58 @@ import GameGrid from '../components/GameGrid';
   useEffect (() => {
     getData()
     .then(data => setData(data[0]))
-  }, [gridState])
-
-  // getData()
-  // .catch(e => console.log(e))
-  // .finally(() => {isLoading = false})
-  // .then(data => setData(data))
-
+  },[])
+  
+  useEffect(() => {
+    if(Object.keys(data).length === 0) return 
+    console.log(`data: ${data}`)
+    getCards();
+  }, [data, gameActive])
+  
   const getCards = () => {
-    // return new Promise((resolve, reject) => {
-    //   if(data.cards.tile_cards){
-    //     resolve(setCards(data.cards.tile_cards))
-    //   } else {
-    //     reject(new(Error("Data failed to load")))
-    //   }
-    // })
     const cardData = data.cards.tile_cards
+    console.log(`card data: ${cardData}`)
     setCards(cardData)
   }
 
+ useEffect(() => {
+    if(!cards || gameActive == false) return
+    console.log(cards)
+    placeCard()
+    setGameActive(false)
+ }, [gameActive])
+
   const placeCard = () => {
+    const row = Math.floor(Math.random()*6);
+    const col = Math.floor(Math.random()*10);
     const tempArr = gridState
-    tempArr[0].splice(0, 1, cards['start-card'])
+    tempArr[row].splice(col, 1, cards['start-card'])
     setGridState(tempArr)
   }
 
-  const setupNewGame = () => {
-    getCards()
-    setLoading(false);
+  // const setupNewGame = () => {
+  //   // getCards()
+  //   setLoading(false);
+  // }
+
+  const handleStartClick = () => {
+    console.log('starting game')
+    setGameActive(true);
   }
 
-  if((loading)){
-    return <div className= "game-container">
-      <Loading setupNewGame={setupNewGame}/>
-    </div>;
-  }
-  else{
+  // if((loading)){
+  //   return <div className= "game-container">
+  //     <Loading setupNewGame={setupNewGame}/>
+  //   </div>;
+  // }
+  // else{
     return (
       <div className= "game-container">
         <GameGrid  gridState={gridState}/>
         <button onClick={handleStartClick}>Start Game</button>
       </div>
     )
-  }
+  // }
 }
 
 export default GameContainer;
